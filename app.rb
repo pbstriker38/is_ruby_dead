62d1f93499a7109baa0481c32136d312f8576f80
Daniel Alfaro <daniel@fair.com>
2020-02-12 14:05:38 -0800

    Alive should be dead, whoops.

--- app.rb
+++ app.rb
@@ -1,5 +1,4 @@
 require 'sinatra'
-# configure { set :server, :puma }
 
 YES_SAD = OpenStruct.new(response: 'Yes 🥺', color: '#1bb535')
 YES_FACEPALM = OpenStruct.new(response: 'Yes 🤦🏾‍♀️', color: '#1bb535')
@@ -8,7 +7,7 @@ MAYBE = OpenStruct.new(response: 'Maybe 🤷🏻‍♂️', color: '#d6d016')
 ANSWERS = [YES_SAD, YES_FACEPALM, NO, MAYBE]
 
 get '/' do
-  @app_icon = settings.production? ? 'https://is-ruby-alive.herokuapp.com/ruby.png' : 'http://localhost:4567/ruby.png'
+  @app_icon = settings.production? ? 'https://is-ruby-dead.herokuapp.com/ruby.png' : 'http://localhost:4567/ruby.png'
   @answer = ANSWERS.sample
   erb :index
 end
