5e9e177ac9e02c8402ba780a64b40e85e2eb894a
Daniel Alfaro <daniel@fair.com>
2020-03-05 11:01:06 -0800

    Move APP_URL to env variable
    - remove unused route and image

--- app.rb
+++ app.rb
@@ -1,10 +1,7 @@
 require 'sinatra'
 require_relative 'answer_randomizer'
 
-APP_URL = settings.production? ? 'https://is-ruby-dead.herokuapp.com' : 'http://localhost:4567'
-
 get '/' do
-  @app_icon = settings.production? ? "#{APP_URL}/ruby.png" : "#{APP_URL}/ruby.png"
   @answer = AnswerRandomizer.random_answer
 
   erb :index
@@ -12,8 +9,4 @@ end
 
 get '/ruby.png' do
   send_file 'ruby.png'
-end
-
-get '/bender_laugh.gif' do
-  send_file 'bender_laugh.gif'
 end
