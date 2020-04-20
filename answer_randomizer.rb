2f0ce383131d9acfa3643c5e6cec25bbe1c0e9ba
Daniel Alfaro <pbstriker38@gmail.com>
2020-04-20 10:10:25 -0700

    Add random sad gif for yes answer

--- answer_randomizer.rb
+++ answer_randomizer.rb
@@ -23,11 +23,16 @@ class AnswerRandomizer
       image: GiphyClient.random_gif(tag: 'no', rating: 'PG-13'),
       color: '#cc4c42'
     ) }
+    random_yes_gif = -> { OpenStruct.new(
+      message: 'Yes',
+      image: GiphyClient.random_gif(tag: 'sad', rating: 'PG-13'),
+      color: '#1bb535'
+    ) }
     random_never_gif = -> { OpenStruct.new(
       message: 'Never',
       image: GiphyClient.random_gif(tag: 'never', rating: 'PG-13'),
       color: '#cc4c42'
     ) }
-    @answer = (ANSWERS + [random_laugh_gif, random_no_gif, random_never_gif]).sample.call
+    @answer = (ANSWERS + [random_laugh_gif, random_no_gif, random_yes_gif, random_never_gif]).sample.call
   end
 end
