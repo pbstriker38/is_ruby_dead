fd8284a60044793af986dd573acfad655d375495
Daniel Alfaro <daniel@software.com>
2021-01-06 15:59:45 -0800

    temp

--- answer_randomizer.rb
+++ answer_randomizer.rb
@@ -34,6 +34,7 @@ class AnswerRandomizer
       color: '#cc4c42'
     ) }
     puts "hello world"
+    puts "hello world"
 
     @answer = (ANSWERS + [random_laugh_gif, random_no_gif, random_yes_gif, random_never_gif]).sample.call
   end
