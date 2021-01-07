336f85ab5b8e47a58d2dce1e9e3c7847b2da413f
Daniel Alfaro <daniel@software.com>
2021-01-06 16:04:44 -0800

    temp

--- app.rb
+++ app.rb
@@ -3,10 +3,10 @@ require_relative 'answer_randomizer'
 
 get '/' do
   @answer = AnswerRandomizer.random_answer
-
   erb :index
 end
 
 get '/ruby.png' do
+  puts 'Hi!!'
   send_file 'ruby.png'
 end
