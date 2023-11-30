dc787f02a9f3a535972d8d38fee4489830d06dba
Daniel Alfaro <pbstriker38@gmail.com>
2023-11-29 20:21:31 -0800

    Add public directory for images
    - Remove google fonts

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: APP_ENV=production RACK_ENV=production ruby app.rb
--- app.rb
+++ app.rb
@@ -2,17 +2,11 @@ require 'sinatra'
 require_relative 'answer_randomizer'
 
 class App < Sinatra::Base
+  set :static_cache_control, [:public, max_age: 300]
+
   get '/' do
     @answer = AnswerRandomizer.random_answer
 
     erb :index
-  end
-
-  get '/ruby.png' do
-    send_file 'ruby.png'
-  end
-
-  get '/github_logo.png' do
-    send_file 'GitHub-Mark-Light-64px.png'
   end
 end
